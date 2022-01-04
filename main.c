f2afb181a2402f3f0d312dc9f57cdf2dd2f3868b
0xDATAWOLF <magiktrikdev@gmail.com>
2022-01-03 21:59:12 -0600

    Windows Platform Update

--- main.c
+++ main.c
@@ -355,6 +355,13 @@ ApplicationMain(void* AppMemory)
 	i32 ROMFilesize = PlatformFilesize(ROM_INVADERS_H);
 	i32 ReadStatus = PlatformReadfile(ROM_INVADERS_H, AppMemory, ROMFilesize);
 
+	/**
+	 * This function will output to the console the entire disassembled binary in a human readable
+	 * format. The downside is that it will output to the console the entire disassembled binary.
+	 * 
+	 * It's not entirely useful except for debug purposes. And it won't work on Windows for various
+	 * reason that I won't go into in a small comment.
+	 */
 	DisassembleBinary(AppMemory, ROMFilesize);
 
 	return 0;
@@ -376,94 +383,15 @@ ApplicationMain(void* AppMemory)
  */
 #ifdef _WIN32
 #include <windows.h>
-
-/**
- * Because, only Microsoft would ever make it this difficult to run a GUI app with a console.
- * https://stackoverflow.com/questions/191842/how-do-i-get-console-output-in-c-with-a-windows-program
- * 
- * These functions are designed specifically for debug output in the console and will be summarily replaced
- * with IMGUI output.
- */
-void
-AdjustConsoleBuffer(int16_t minLength)
-{
-    // Set the screen buffer to be big enough to scroll some text
-    CONSOLE_SCREEN_BUFFER_INFO conInfo;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &conInfo);
-    if (conInfo.dwSize.Y < minLength)
-        conInfo.dwSize.Y = minLength;
-    SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), conInfo.dwSize);
-}
-
-bool
-RedirectConsoleIO()
-{
-	bool result = true;
-	FILE *fp;
-
-	// Redirect STDIN if the console has an input handle
-	if (GetStdHandle(STD_INPUT_HANDLE) != INVALID_HANDLE_VALUE)
-		if (freopen_s(&fp, "CONIN$", "r", stdin) != 0)
-			result = false;
-		else
-			setvbuf(stdin, NULL, _IONBF, 0);
-
-	// Redirect STDOUT if the console has an output handle
-	if (GetStdHandle(STD_OUTPUT_HANDLE) != INVALID_HANDLE_VALUE)
-		if (freopen_s(&fp, "CONOUT$", "w", stdout) != 0)
-			result = false;
-		else
-			setvbuf(stdout, NULL, _IONBF, 0);
-
-	// Redirect STDERR if the console has an error handle
-	if (GetStdHandle(STD_ERROR_HANDLE) != INVALID_HANDLE_VALUE)
-		if (freopen_s(&fp, "CONOUT$", "w", stderr) != 0)
-			result = false;
-		else
-			setvbuf(stderr, NULL, _IONBF, 0);
-
-	return result;
-}
-
-bool
-ReleaseConsole()
-{
-	bool result = true;
-	FILE *fp;
-
-	// Just to be safe, redirect standard IO to NUL before releasing.
-
-	// Redirect STDIN to NUL
-	if (freopen_s(&fp, "NUL:", "r", stdin) != 0)
-		result = false;
-	else
-		setvbuf(stdin, NULL, _IONBF, 0);
-
-	// Redirect STDOUT to NUL
-	if (freopen_s(&fp, "NUL:", "w", stdout) != 0)
-		result = false;
-	else
-		setvbuf(stdout, NULL, _IONBF, 0);
-
-	// Redirect STDERR to NUL
-	if (freopen_s(&fp, "NUL:", "w", stderr) != 0)
-		result = false;
-	else
-		setvbuf(stderr, NULL, _IONBF, 0);
-
-	// Detach from console
-	if (!FreeConsole())
-		result = false;
-
-	return result;
-}
+#include <stdio.h>
+#include <io.h>
 
 i32
 WIN32Readfile(const char* Filepath, void* MemoryLoc, i32 BytesReading)
 {
 
-	HANDLE fileHandle = CreateFile(Filepath, GENERIC_READ, FILE_SHARE_READ,
-		NULL, OPEN_EXISTING, NULL, NULL);
+	HANDLE fileHandle = CreateFileA(Filepath, GENERIC_READ, FILE_SHARE_READ,
+		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
 	/**
 	 * NOTE: We are enforcing ReadFile to always read the bytes requested,
@@ -476,14 +404,16 @@ WIN32Readfile(const char* Filepath, void* MemoryLoc, i32 BytesReading)
 
 	assert(BytesRead == BytesReading);
 
+	return 0;
+
 }
 
 i32
 WIN32Filesize(const char* Filepath)
 {
 
-	HANDLE fileHandle = CreateFile(Filepath, GENERIC_READ, FILE_SHARE_READ,
-		NULL, OPEN_EXISTING, NULL, NULL);
+	HANDLE fileHandle = CreateFileA(Filepath, GENERIC_READ, FILE_SHARE_READ,
+		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
 	assert(fileHandle != INVALID_HANDLE_VALUE);
 
@@ -503,14 +433,6 @@ WIN32Filesize(const char* Filepath)
 i32 WINAPI
 wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR Commandline, i32 CommandShow)
 {
-
-    if (AttachConsole(ATTACH_PARENT_PROCESS))
-    {
-		AdjustConsoleBuffer(1024);
-        RedirectConsoleIO();
-        printf("\n");
-
-    }
 
 	void* ApplicationMemory = malloc(Megabytes(512));
 	PlatformFilesize = WIN32Filesize;
